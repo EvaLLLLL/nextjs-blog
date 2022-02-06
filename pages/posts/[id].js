64f13caf55d46f74fa37262b8dcf94380a3d20c6
lyhhh <17610603221@163.com>
2022-02-07 00:17:47 +0800

    lint

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -5,20 +5,21 @@ import Date from "../../components/date";
 import utilStyles from "../../styles/utils.module.css";
 
 export default function Post({ postData, params }) {
-  return;
-  <Layout>
-    <Head>
-      <title>{postData.title}</title>
-    </Head>
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
 
-    <article>
-      <h1 className={utilStyles.headingXl}>{postData.title}</h1>
-      <div className={utilStyles.lightText}>
-        <Date dateString={postData.date} />
-      </div>
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-    </article>
-  </Layout>;
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  );
 }
 
 export async function getStaticPaths() {
